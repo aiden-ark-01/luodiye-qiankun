f31ae5708b6450fa4fe63b089bac351cc9192c4f
wkf888 <kang_0726@aliyun.com>
2021-09-23 00:56:16 +0800

    服务器配置

--- vue-app/src/config/index.js
+++ vue-app/src/config/index.js
@@ -2,8 +2,8 @@ export default [
   {
     //获取主应用传入的路由名称,并接入vuerouter
     path: '/',
-    name: 'model3',
+    name: 'model1',
     //动态懒加载组件
-    component: () => import(/* webpackChunkName: "about" */ `../views/models/model3`)
+    component: () => import(/* webpackChunkName: "about" */ `../views/models/model1`)
   },
 ]
